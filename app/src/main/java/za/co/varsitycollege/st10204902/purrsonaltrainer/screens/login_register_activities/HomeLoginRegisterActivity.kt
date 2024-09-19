95d867d25e112600bfb56113960047704b2c27a9
Anneme <ST10144453@vcconnect.edu.za>
2024-09-19 21:43:50 +0200

    Complete Landing Page
    NOTE: I attempted to set the startup screen to the landing but i couldn't get it to work so i didn't want to fuck with it too much.
    
    I know that i probably didn't implement responsive development that well, I'm new to it and I couldn't really understand the resources that I found... will consult Nick on help if he knows anything but i wanted to get as much of a headstart on the UI that I can :)

--- app/src/main/java/za/co/varsitycollege/st10204902/purrsonaltrainer/screens/login_register_activities/HomeLoginRegisterActivity.kt
+++ app/src/main/java/za/co/varsitycollege/st10204902/purrsonaltrainer/screens/login_register_activities/HomeLoginRegisterActivity.kt
@@ -1,18 +1,39 @@
 package za.co.varsitycollege.st10204902.purrsonaltrainer.screens.login_register_activities
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import za.co.varsitycollege.st10204902.purrsonaltrainer.R
 import za.co.varsitycollege.st10204902.purrsonaltrainer.databinding.ActivityHomeBinding
+import za.co.varsitycollege.st10204902.purrsonaltrainer.frontend_logic.BubbleButton
 
 class HomeLoginRegisterActivity : AppCompatActivity() {
-    //THIS IS THE FIRST PAGE IN UI FLOW
+    // THIS IS THE FIRST PAGE IN UI FLOW
     private lateinit var binding: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        binding = ActivityHomeBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_home_login_register)
+
+        val loginButton: BubbleButton = findViewById(R.id.loginButton)
+        val registerButton: BubbleButton = findViewById(R.id.registerButton)
+        val ssoButton: BubbleButton = findViewById(R.id.ssoButton)
+
+
+        // Customize each button with different properties
+        customizeButton(loginButton, R.color.login_yellow_gradient, R.color.login_orange, R.string.login)
+        customizeButton(registerButton, R.color.register_purple_gradient, R.color.register_purple, R.string.register)
+        customizeButton(ssoButton, R.color.sso_gradient_start, R.color.sso_gradient_end, R.string.google_sso)
+    }
+
+    /**
+     * Customizes the appearance and properties of a BubbleButton.
+     *
+     * @param button The BubbleButton to be customized.
+     * @param startColorRes The resource ID of the start color for the gradient.
+     * @param endColorRes The resource ID of the end color for the gradient.
+     * @param textRes The resource ID of the text to be set on the button.
+     */
+    private fun customizeButton(button: BubbleButton, startColorRes: Int, endColorRes: Int, textRes: Int) {
+        button.setButtonProperties(startColorRes, endColorRes, textRes)
     }
 }
