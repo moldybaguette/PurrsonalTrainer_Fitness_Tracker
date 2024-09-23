bfb3a5b93b32b2cf6be678d751f8021791fe8aaf
Anneme <ST10144453@vcconnect.edu.za>
2024-09-24 00:17:45 +0200

    fully done view + navigation to view , just need the combo box

--- app/src/main/java/za/co/varsitycollege/st10204902/purrsonaltrainer/screens/fragments/RoutinesFragment.kt
+++ app/src/main/java/za/co/varsitycollege/st10204902/purrsonaltrainer/screens/fragments/RoutinesFragment.kt
@@ -1,32 +1,23 @@
 package za.co.varsitycollege.st10204902.purrsonaltrainer.screens.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import za.co.varsitycollege.st10204902.purrsonaltrainer.R
+import za.co.varsitycollege.st10204902.purrsonaltrainer.screens.workout_activities.CreateRoutineActivity
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RoutinesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RoutinesFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
@@ -38,22 +29,21 @@ class RoutinesFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_routines, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val makeNewRoutineButton: ImageButton = view.findViewById(R.id.add_button)
+        makeNewRoutineButton.setOnClickListener {
+            val intent = Intent(requireActivity(), CreateRoutineActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RoutinesFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             RoutinesFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
                 }
             }
     }
