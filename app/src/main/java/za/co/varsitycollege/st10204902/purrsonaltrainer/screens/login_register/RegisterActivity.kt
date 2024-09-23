550ebb01e9bd67d4e92dc187f266c34f50578b04
Anneme <ST10144453@vcconnect.edu.za>
2024-09-23 21:15:03 +0200

    fully completed register UI

--- app/src/main/java/za/co/varsitycollege/st10204902/purrsonaltrainer/screens/login_register/RegisterActivity.kt
+++ app/src/main/java/za/co/varsitycollege/st10204902/purrsonaltrainer/screens/login_register/RegisterActivity.kt
@@ -1,10 +1,29 @@
 package za.co.varsitycollege.st10204902.purrsonaltrainer.screens.login_register
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import za.co.varsitycollege.st10204902.purrsonaltrainer.R
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register) //navigating to the register view
+
+        val registerButton: AppCompatButton = findViewById(R.id.registerButton) //getting the register button
+        val originalBackground = registerButton.background //getting the original background of the button
+
+        // Set the on click listener for the register button
+        registerButton.setOnClickListener {
+            registerButton.setBackgroundResource(R.drawable.svg_purple_bblbtn_clicked) // Set the background to the clicked background
+
+            // Revert the background after 2 seconds (2000 milliseconds)
+            Handler(Looper.getMainLooper()).postDelayed({
+                registerButton.background = originalBackground
+            }, 400)
+        }
     }
 }
