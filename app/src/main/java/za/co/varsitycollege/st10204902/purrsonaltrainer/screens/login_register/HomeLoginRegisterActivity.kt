1356b4238bf6fe24dbe3ba9c45aa4dd107409c33
ST10180919 <st10180919@vcconnect.edu.za>
2024-09-23 20:49:00 +0200

    Added Login navigation from the login register activity

--- app/src/main/java/za/co/varsitycollege/st10204902/purrsonaltrainer/screens/login_register/HomeLoginRegisterActivity.kt
+++ app/src/main/java/za/co/varsitycollege/st10204902/purrsonaltrainer/screens/login_register/HomeLoginRegisterActivity.kt
@@ -1,19 +1,35 @@
 package za.co.varsitycollege.st10204902.purrsonaltrainer.screens.login_register
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import za.co.varsitycollege.st10204902.purrsonaltrainer.R
 import za.co.varsitycollege.st10204902.purrsonaltrainer.databinding.ActivityHomeBinding
+import za.co.varsitycollege.st10204902.purrsonaltrainer.databinding.ActivityHomeLoginRegisterBinding
 import za.co.varsitycollege.st10204902.purrsonaltrainer.frontend_logic.BubbleButton
 
 class HomeLoginRegisterActivity : AppCompatActivity() {
     // THIS IS THE FIRST PAGE IN UI FLOW
-    private lateinit var binding: ActivityHomeBinding
+    private lateinit var binding: ActivityHomeLoginRegisterBinding
 
     //TODO Check that the multiple button logic works, need to implement the navigation to check that this logic works.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home_login_register)
+        binding = ActivityHomeLoginRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        // Apply login fragment before hand
+        this.supportFragmentManager.beginTransaction().apply {
+            replace(binding.loginFragmentContainer.id, LoginFragment())
+            addToBackStack(null)
+            commit()
+        }
+
+        // Show popup
+        binding.loginButton.setOnClickListener {
+            // Show popup
+            binding.loginFragmentContainer.visibility = View.VISIBLE
+        }
     }
 }
