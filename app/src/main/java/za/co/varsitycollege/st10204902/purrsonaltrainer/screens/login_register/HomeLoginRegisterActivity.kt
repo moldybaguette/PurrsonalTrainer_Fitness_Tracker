1c61a4b15699a9a62da2cb0f533705727c961bb9
Try-Angle1134 <Jaspervann1313@gmail.com>
2024-09-23 19:18:29 +0200

    stash

--- app/src/main/java/za/co/varsitycollege/st10204902/purrsonaltrainer/screens/login_register/HomeLoginRegisterActivity.kt
+++ app/src/main/java/za/co/varsitycollege/st10204902/purrsonaltrainer/screens/login_register/HomeLoginRegisterActivity.kt
@@ -1,19 +1,54 @@
 package za.co.varsitycollege.st10204902.purrsonaltrainer.screens.login_register
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import za.co.varsitycollege.st10204902.purrsonaltrainer.R
+import za.co.varsitycollege.st10204902.purrsonaltrainer.backend.AuthManager
 import za.co.varsitycollege.st10204902.purrsonaltrainer.databinding.ActivityHomeBinding
 import za.co.varsitycollege.st10204902.purrsonaltrainer.frontend_logic.BubbleButton
+import za.co.varsitycollege.st10204902.purrsonaltrainer.services.navigateTo
 
 class HomeLoginRegisterActivity : AppCompatActivity() {
     // THIS IS THE FIRST PAGE IN UI FLOW
     private lateinit var binding: ActivityHomeBinding
 
-    //TODO Check that the multiple button logic works, need to implement the navigation to check that this logic works.
+    //TODO Check that the multiple button logic works, need to implement the navigation to check that this logic works
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_login_register)
 
+
+
+        //buttons
+        var loginButton = findViewById<AppCompatButton>(R.id.loginButton)
+        var registerButton = findViewById<AppCompatButton>(R.id.registerButton)
+        var SSOButton = findViewById<AppCompatButton>(R.id.ssoButton)
+
+        //onclick listeners
+        loginButton.setOnClickListener {
+            //navigateTo(this,LoginFragment::class.java, null)
+            Toast.makeText(this@HomeLoginRegisterActivity, "feature not ready", Toast.LENGTH_SHORT).show()
+        }
+        registerButton.setOnClickListener {
+            navigateTo(this,RegisterActivity::class.java, null)
+        }
+        SSOButton.setOnClickListener {
+            val googleIdOption: GetGoogleIdOption = GetGoogleIdOption.Builder()
+                .setFilterByAuthorizedAccounts(true)
+                .setServerClientId(getString(R.string.your_web_client_id))
+                .setAutoSelectEnabled(true)
+                .setNonce("1234567890123456")
+                .build()
+        }
     }
 }
+
--- app/src/main/java/za/co/varsitycollege/st10204902/purrsonaltrainer/screens/login_register/RegisterActivity.kt
+++ app/src/main/java/za/co/varsitycollege/st10204902/purrsonaltrainer/screens/login_register/RegisterActivity.kt
@@ -1,10 +1,48 @@
 package za.co.varsitycollege.st10204902.purrsonaltrainer.screens.login_register
 
 import android.os.Bundle
+import android.os.UserManager
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import za.co.varsitycollege.st10204902.purrsonaltrainer.R
+import za.co.varsitycollege.st10204902.purrsonaltrainer.backend.AuthManager
+import za.co.varsitycollege.st10204902.purrsonaltrainer.screens.HomeActivity
+import za.co.varsitycollege.st10204902.purrsonaltrainer.services.navigateTo
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Set the layout for the activity
+        var email = findViewById<EditText>(R.id.emailInput)
+        var password = findViewById<EditText>(R.id.passwordInput)
+        var confirmPassword = findViewById<EditText>(R.id.passwordConfirmInput)
+        var registerButton = findViewById<EditText>(R.id.registerButton)
+
+        //onclick listeners
+        registerButton.setOnClickListener {
+            // Check if the passwords match
+            if (password.text.toString() == confirmPassword.text.toString()) {
+                // Register the user
+                CoroutineScope(Dispatchers.IO).launch {
+                    var authManager = AuthManager()
+                    val result = authManager.registerUser(email.text.toString(), password.text.toString())
+                    if (result.isSuccess) {
+                        // Navigate to the next screen
+                        navigateTo(this@RegisterActivity, HomeActivity::class.java, null)
+                    } else {
+                        // Display an error message
+                        Toast.makeText(this@RegisterActivity, "Error registering user", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } else {
+                // Display an error message
+                Toast.makeText(this@RegisterActivity, "Passwords do not match", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
