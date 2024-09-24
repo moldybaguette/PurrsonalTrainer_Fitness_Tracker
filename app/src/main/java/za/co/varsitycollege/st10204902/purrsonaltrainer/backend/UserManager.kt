621f138e8af19d55c18d6434fb3edcacb2fd17db
ST10195824 <ST10195824@vcconnect.edu.za>
2024-09-24 13:58:15 +0200

    atuff

--- app/src/main/java/za/co/varsitycollege/st10204902/purrsonaltrainer/backend/UserManager.kt
+++ app/src/main/java/za/co/varsitycollege/st10204902/purrsonaltrainer/backend/UserManager.kt
@@ -1,10 +1,7 @@
 package za.co.varsitycollege.st10204902.purrsonaltrainer.backend
 
 import android.util.Log
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
