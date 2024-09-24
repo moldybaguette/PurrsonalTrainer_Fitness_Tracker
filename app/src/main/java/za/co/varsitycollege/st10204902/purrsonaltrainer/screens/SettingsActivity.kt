d5f9f217598bd57f6071035cf41d513110953bee
ST10204902 <ST10204902@vcconnect.edu.za>
2024-09-24 17:06:18 +0200

    Added input disabling for google email firebase users Updated account details xml for extra accessibility Added account details button on click listener to navigate to AccountDetailsActivity.kt Added new strings to strings.xml

--- app/src/main/java/za/co/varsitycollege/st10204902/purrsonaltrainer/screens/SettingsActivity.kt
+++ app/src/main/java/za/co/varsitycollege/st10204902/purrsonaltrainer/screens/SettingsActivity.kt
@@ -3,10 +3,6 @@ package za.co.varsitycollege.st10204902.purrsonaltrainer.screens
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.google.firebase.auth.FirebaseAuth
-import za.co.varsitycollege.st10204902.purrsonaltrainer.R
 import za.co.varsitycollege.st10204902.purrsonaltrainer.databinding.ActivitySettingsBinding
 import za.co.varsitycollege.st10204902.purrsonaltrainer.screens.settings_activities.AccountDetailsActivity
 import za.co.varsitycollege.st10204902.purrsonaltrainer.services.navigateTo
