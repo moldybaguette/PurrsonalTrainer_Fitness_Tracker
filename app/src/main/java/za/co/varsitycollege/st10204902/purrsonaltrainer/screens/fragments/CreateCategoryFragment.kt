f09dfefefb214002a4477aefb0335bdd7b634b74
ST10180919 <st10180919@vcconnect.edu.za>
2024-09-25 18:21:06 +0200

    Added dismiss on category name added

--- app/src/main/java/za/co/varsitycollege/st10204902/purrsonaltrainer/screens/fragments/CreateCategoryFragment.kt
+++ app/src/main/java/za/co/varsitycollege/st10204902/purrsonaltrainer/screens/fragments/CreateCategoryFragment.kt
@@ -10,28 +10,14 @@ import android.view.inputmethod.EditorInfo
 import android.widget.EditText
 import za.co.varsitycollege.st10204902.purrsonaltrainer.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Use the [CreateCategoryFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
 class CreateCategoryFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var dismissAction: () -> Unit
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -49,6 +35,10 @@ class CreateCategoryFragment : Fragment() {
                 val textFromUser = categoryNameInput.text
                 // Do something with the category name here
 
+                // Remove focus from the EditText
+                categoryNameInput.clearFocus()
+
+                dismissAction()
                 true
             } else {
                 false
@@ -57,23 +47,8 @@ class CreateCategoryFragment : Fragment() {
         return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CreateCategoryFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CreateCategoryFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    public fun setDismissAction(dismissAction: () -> Unit)
+    {
+        this.dismissAction = dismissAction
     }
 }
