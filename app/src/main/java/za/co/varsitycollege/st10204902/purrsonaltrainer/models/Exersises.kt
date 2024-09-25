0f3af8811f74e4fea8ea861c6ad4a49c9e133cea
ST10195824 <ST10195824@vcconnect.edu.za>
2024-09-25 12:14:07 +0200

    added method to get all exercises in a category

--- app/src/main/java/za/co/varsitycollege/st10204902/purrsonaltrainer/models/Exersises.kt
+++
@@ -1,8 +0,0 @@
-package za.co.varsitycollege.st10204902.purrsonaltrainer.models
-
-data class Exersises(
-    val exercise_id: String,
-    val ExerciseName: String,
-    val Category: String,
-    val Notes: String
-)
