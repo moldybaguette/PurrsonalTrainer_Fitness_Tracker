2f624da07fce0c25f33e4e6c34d4f52dae34f109
ST10180919 <st10180919@vcconnect.edu.za>
2024-09-25 17:43:04 +0200

    Added method of capture category from user

--- app/src/main/java/za/co/varsitycollege/st10204902/purrsonaltrainer/screens/fragments/CreateCategoryFragment.kt
+++ app/src/main/java/za/co/varsitycollege/st10204902/purrsonaltrainer/screens/fragments/CreateCategoryFragment.kt
@@ -1,10 +1,13 @@
 package za.co.varsitycollege.st10204902.purrsonaltrainer.screens.fragments
 
 import android.os.Bundle
+import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
 import za.co.varsitycollege.st10204902.purrsonaltrainer.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -35,7 +38,23 @@ class CreateCategoryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_category, container, false)
+        val view =  inflater.inflate(R.layout.fragment_create_category, container, false)
+
+        // Getting the category name from user, made by chat but tested and works
+        val categoryNameInput = view.findViewById<EditText>(R.id.customCategoryName)
+        categoryNameInput.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE ||
+                event != null && event.keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_DOWN) {
+
+                val textFromUser = categoryNameInput.text
+                // Do something with the category name here
+
+                true
+            } else {
+                false
+            }
+        }
+        return view
     }
 
     companion object {
