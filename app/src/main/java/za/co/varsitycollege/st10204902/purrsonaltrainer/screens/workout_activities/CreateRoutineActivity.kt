0c3e75f2f33c24b05f0026c3de9e19367f6c3a8a
Try-Angle1134 <Jaspervann1313@gmail.com>
2024-09-25 11:48:15 +0200

    Exersises and catagory adapter

--- app/src/main/java/za/co/varsitycollege/st10204902/purrsonaltrainer/screens/workout_activities/CreateRoutineActivity.kt
+++ app/src/main/java/za/co/varsitycollege/st10204902/purrsonaltrainer/screens/workout_activities/CreateRoutineActivity.kt
@@ -17,6 +17,7 @@ import za.co.varsitycollege.st10204902.purrsonaltrainer.screens.fragments.Choose
 class CreateRoutineActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCreateRoutineBinding
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
