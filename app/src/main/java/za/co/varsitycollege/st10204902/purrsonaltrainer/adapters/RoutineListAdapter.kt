83e41d82dafc6754297f6480cc4fbb7b976c170e
Anneme <ST10144453@vcconnect.edu.za>
2024-09-27 14:55:10 +0200

    more updates

--- app/src/main/java/za/co/varsitycollege/st10204902/purrsonaltrainer/adapters/RoutineListAdapter.kt
+++ app/src/main/java/za/co/varsitycollege/st10204902/purrsonaltrainer/adapters/RoutineListAdapter.kt
@@ -9,6 +9,7 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentManager
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import za.co.varsitycollege.st10204902.purrsonaltrainer.R
--- app/src/main/java/za/co/varsitycollege/st10204902/purrsonaltrainer/screens/SettingsActivity.kt
+++ app/src/main/java/za/co/varsitycollege/st10204902/purrsonaltrainer/screens/SettingsActivity.kt
@@ -1,6 +1,10 @@
 package za.co.varsitycollege.st10204902.purrsonaltrainer.screens
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.animation.AnimationUtils
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -21,20 +25,39 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        soundManager = SoundManager(this, R.raw.custom_tap_sound)
         binding = ActivitySettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        soundManager = SoundManager(this, R.raw.custom_tap_sound)
 
+        applyFloatUpAnimation(binding.settingsTitle)
+        applyFloatUpAnimation(binding.accountButton)
+        applyFloatUpAnimation(binding.catSettingsButton)
+        applyFloatUpAnimation(binding.statisticsButton)
 
+        binding.accountButton.setOnClickListener() {
+            soundManager.playSound()
+            val originalBackgroundLogin = binding.accountButton.background
+            binding.accountButton.setBackgroundResource(R.drawable.svg_orange_bblbtn_clicked)
+
+            // Revert the background after 2 seconds (2000 milliseconds)
+            Handler(Looper.getMainLooper()).postDelayed({
+                binding.accountButton.background = originalBackgroundLogin
+            }, 400)
         binding.accountButton.setOnClickListener(){
             soundManager.playSound()
             binding.accountButton.setBackgroundResource(R.drawable.svg_orange_bblbtn_clicked)
             navigateTo(this, AccountDetailsActivity::class.java, null)
         }
 
-        binding.catSettingsButton.setOnClickListener(){
+        binding.catSettingsButton.setOnClickListener() {
             soundManager.playSound()
+            val originalBackgroundCatSettings = binding.catSettingsButton.background
             binding.catSettingsButton.setBackgroundResource(R.drawable.svg_green_bblbtn_clicked)
+
+            // Revert the background after 2 seconds (2000 milliseconds)
+            Handler(Looper.getMainLooper()).postDelayed({
+                binding.catSettingsButton.background = originalBackgroundCatSettings
+            }, 400)
             Toast.makeText(this, "Cat Settings is not yet implemented", Toast.LENGTH_SHORT).show()
             navigateTo(this, CatSettingsActivity::class.java, null)
         }
@@ -42,14 +65,31 @@ class SettingsActivity : AppCompatActivity() {
         binding.statisticsButton.setOnClickListener(){
             soundManager.playSound()
             binding.statisticsButton.setBackgroundResource(R.drawable.svg_purple_bblbtn_clicked)
+            soundManager.playSound()
+            val originalBackgroundStatistics = binding.statisticsButton.background
+            binding.statisticsButton.setBackgroundResource(R.drawable.svg_purple_bblbtn_clicked)
+
+            // Revert the background after 2 seconds (2000 milliseconds)
+            Handler(Looper.getMainLooper()).postDelayed({
+                binding.statisticsButton.background = originalBackgroundStatistics
+            }, 400)
+            navigateTo(this, AccountDetailsActivity::class.java, null)
+        }
             navigateTo(this, BreakdownActivity::class.java, null)
         }
 
-    }
+}
 
     override fun onBackPressed() {
         super.onBackPressed()
         navigateTo(this, HomeActivity::class.java, null)
         finish()
+    }
+
+    private fun applyFloatUpAnimation(view: View?) {
+        view?.let {
+            val animation = AnimationUtils.loadAnimation(this, R.anim.float_up)
+            it.startAnimation(animation)
+        }
     }
 }
